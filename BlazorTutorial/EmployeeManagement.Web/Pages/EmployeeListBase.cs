fdcf4b0d217def051b8093e56f9001f35933e982
rayjung95 <rayjung95@gmail.com>
2020-12-29 13:18:15 -0800

    Added api project

--- BlazorTutorial/EmployeeManagement.Web/Pages/EmployeeListBase.cs
+++ BlazorTutorial/EmployeeManagement.Web/Pages/EmployeeListBase.cs
@@ -31,7 +31,7 @@ namespace EmployeeManagement.Web.Pages
                 Email = "David@pragimtech.com",
                 DateOfBrith = new DateTime(1980, 10, 5),
                 Gender = Gender.Male,
-                Department = new Department { DepartmentId = 1, DepartmentName = "IT" },
+                DepartmentId = 1,
                 PhotoPath = "images/John.png"
             };
             Employee e2 = new Employee
@@ -42,7 +42,7 @@ namespace EmployeeManagement.Web.Pages
                 Email = "Sam@pragimtech.com",
                 DateOfBrith = new DateTime(1981, 12, 22),
                 Gender = Gender.Male,
-                Department = new Department { DepartmentId = 2, DepartmentName = "HR" },
+                DepartmentId = 2,
                 PhotoPath = "images/Sam.png"
             };
             Employee e3 = new Employee
@@ -53,7 +53,7 @@ namespace EmployeeManagement.Web.Pages
                 Email = "mary@pragimtech.com",
                 DateOfBrith = new DateTime(1979, 11, 11),
                 Gender = Gender.Female,
-                Department = new Department { DepartmentId = 1, DepartmentName = "IT" },
+                DepartmentId = 1,
                 PhotoPath = "images/Maria.png"
             };
             Employee e4 = new Employee
@@ -64,7 +64,7 @@ namespace EmployeeManagement.Web.Pages
                 Email = "sara@pragimtech.com",
                 DateOfBrith = new DateTime(1982, 9, 23),
                 Gender = Gender.Female,
-                Department = new Department { DepartmentId = 3, DepartmentName = "Payroll" },
+                DepartmentId = 3,
                 PhotoPath = "images/Jack.png"
             };
             Employees = new List<Employee> { e1, e2, e3, e4 };
